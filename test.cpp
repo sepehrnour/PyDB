6ea82a6057dcdf97415f31e205aed4f2363afabe
sepehrnour <nourbakhshsepehr@gmail.com>
2023-02-22 15:09:48 -0800

    test.cpp update

--- test.cpp
+++ test.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main ()
 {
     int n = 10, sum = 0;
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i <= n; i++)
     {
-        sum ++;
+        sum++;
     }
-    return sum;
+    cout << sum;
+    return 0;
 }
